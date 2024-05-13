1f599cfc65c3a32508c7f7b19653962feedd62a7
Nachopex <luisignaciomartinezneira@gmail.com>
2024-05-12 23:45:20 -0400

    Se modifico la clase reunion agregando los metodos get horaInicio y horaFin, tambien se modifico los metodos calcularTiempoReal, iniciar y finalizar

--- src/main/java/doo/tarea2/Reunion.java
+++ src/main/java/doo/tarea2/Reunion.java
@@ -1,4 +1,37 @@
 package doo.tarea2;
+import java.time.*;
+import java.util.List;
 
 public class Reunion {
+
+    private int fecha;
+    private Instant horaPrevista;
+    private Duration duracionPrevista;
+    private  Instant horaInicio;
+    private  Instant horaFin;
+
+    /*public List Asistencia(){}
+    public List obtenerAusencias(){}
+
+    public List obetenerRetrasos(){}
+    public int obtenerTotalAsistencias(){}
+
+    public int obtenerPorcentajeAsistencia(){}*/
+    public float calcularTiempoReal(Instant a, Instant b){
+        return b.getEpochSecond()-a.getEpochSecond();
+    }
+    public void iniciar(){
+        horaInicio = Instant.now();
+        System.out.println("La reunio empezo a las "+horaInicio);
+    }
+    public void finalizar(){
+        horaFin = Instant.now();
+        System.out.println("La reunio finalizo a las "+horaFin);
+    }
+    public Instant gethoraInicio(){
+        return horaInicio;
+    }
+    public Instant gethoraFin(){
+        return horaFin;
+    }
 }
