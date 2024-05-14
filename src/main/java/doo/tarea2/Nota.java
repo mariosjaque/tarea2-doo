a852cc9dd69b9e64287e3a8b63d8698b51eb9c4c
Santiago Alexxander Diaz Barra <carldiaz2023@udec.cl>
2024-05-14 15:19:58 -0400

    Se crea la clase Nota, se le agrega el contenido de tipo String y se crea un constructor para esta misma.

--- src/main/java/doo/tarea2/Nota.java
+++ src/main/java/doo/tarea2/Nota.java
@@ -1,4 +1,13 @@
 package doo.tarea2;
 
 public class Nota {
+    private String contenido;
+
+    public Nota(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String contenido() {
+        return contenido;
+    }
 }
