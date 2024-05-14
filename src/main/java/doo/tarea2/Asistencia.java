38f78e0fa7ff86a4372f9741ec8e48b8366cd9f6
Santiago Alexander Diaz Barra <carldiaz2023@udec.cl>
2024-05-14 14:34:26 -0400

    Agrego a la branch santi los codigos faltantes de Empleado, Departamento y Asistencia

--- src/main/java/doo/tarea2/Asistencia.java
+++ src/main/java/doo/tarea2/Asistencia.java
@@ -1,4 +1,12 @@
 package doo.tarea2;
 
+import java.util.ArrayList;
+
 public class Asistencia {
+
+    private ArrayList<String> Lista;
+
+    public ArrayList<String> getLista(){
+        return Lista;
+    }
 }
--- src/main/java/doo/tarea2/Empleado.java
+++ src/main/java/doo/tarea2/Empleado.java
@@ -1,4 +1,16 @@
 package doo.tarea2;
 
 public class Empleado {
+    private String id;
+    private String apellidos;
+    private String nombre;
+    private String correo;
+
+    public String getNombre(){
+        return nombre;
+    }
+    public String getApellidos(){
+        return apellidos;
+    }
+
 }
