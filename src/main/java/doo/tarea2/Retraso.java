96af39e32544557e618ac4a744e4d0e0c59bfb76
Santiago Alexxander Diaz Barra <carldiaz2023@udec.cl>
2024-05-14 16:14:23 -0400

    Se crea la clase Retraso, se le incluye Empleado y Hora para saber quien llegó tarde y a que hora.

--- src/main/java/doo/tarea2/Retraso.java
+++ src/main/java/doo/tarea2/Retraso.java
@@ -1,4 +1,11 @@
 package doo.tarea2;
 
 public class Retraso {
+    private Empleado empleado;
+    private Hora horaLlegada;
+
+        public Retraso(Empleado empleado, Hora horaLlegada) {
+        this.empleado = empleado;
+        this.horaLlegada = horaLlegada;
+    }
 }
