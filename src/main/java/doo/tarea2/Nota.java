e375447f32b7fafb11c00f5036fa057b23eabdf4
Santiago Alexxander Diaz Barra <carldiaz2023@udec.cl>
2024-05-14 17:01:00 -0400

    Clase Nota ahora posee atributo de modificacion. Esto permite que usando getContenido() y setContenido(), se retorne o modifique el contenido de la Nota.

--- src/main/java/doo/tarea2/Nota.java
+++ src/main/java/doo/tarea2/Nota.java
@@ -2,12 +2,13 @@ package doo.tarea2;
 
 public class Nota {
     private String contenido;
-
     public Nota(String contenido) {
         this.contenido = contenido;
     }
-
-    public String contenido() {
+    public String getContenido() {
         return contenido;
+    }
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
     }
 }
