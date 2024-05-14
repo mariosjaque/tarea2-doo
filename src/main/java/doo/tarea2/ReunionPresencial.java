d015816f838fb256c63b586104dcd7e171ae771e
Santiago Alexxander Diaz Barra <carldiaz2023@udec.cl>
2024-05-14 16:46:42 -0400

    La clase ReunionPresencial también hereda la clase abstracta Reunion. Permite modelar reuniones prescenciales con un enlace específico usando los métodos getSala() y modSala() para acceder y modificar la sala de reunión.

--- src/main/java/doo/tarea2/ReunionPresencial.java
+++ src/main/java/doo/tarea2/ReunionPresencial.java
@@ -1,4 +1,14 @@
 package doo.tarea2;
 
-public class ReunionPresencial {
+public class ReunionPresencial extends Reunion {
+    private String sala;
+    public ReunionPresencial(String sala) {
+        this.sala = sala;
+    }
+    public String getSala() {
+        return sala;
+    }
+    public void modSala(String sala) {
+        this.sala = sala;
+    }
 }
